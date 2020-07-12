3156c54a0865fb117d7a66eaa6cbab697a87dfe4
Dipo Arowona <abdulsalam.arowona@gmail.com>
2020-07-12 01:31:51 -0400

    completed UI of landing page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
 
   const searchRequest = (e) => {
     e.preventDefault()
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/album/%7Bid%7D",{
-      "method": "GET",
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem",{
       "headers": {
         "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
         "x-rapidapi-key": "491f353b91msh4d6e560099cab14p16dc45jsn2ebbe77bc738"
@@ -16,8 +15,8 @@ function App() {
     })
       .then(response => response.json())
       .then(data =>{
-        setResults(data)
-        console.log(data)
+        setResults(data.data)
+        console.log(data.data)
       })
   }
 
@@ -28,11 +27,13 @@ function App() {
         <h1 id="landing-text">MUSIQ</h1>
         <form onSubmit={searchRequest}>
           <input type="text" placeholder="Artist,Album,Song..." />
-          <br/>
-          <button>Search</button>
-          {/* {results.map(value => (<h4>{value.title}</h4>))} */}
+          <button style={{margin:'auto'}}>Search</button>
+          {results.map(value => (<h4>{value.title}</h4>))}
       </form>
       </div>
+
+
+
     </div>
   );
 
